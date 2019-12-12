8a0e4b3a3677325351f5e6eaef662cefdab9bd5b
fabian.ludwig@iontis.de <fabian.ludwig@iontis.de>
2019-12-13 00:57:01 +0100

    changed LightDevice Component TODO: on_state initialisation in view is random!

--- src/app/light/light.component.ts
+++ src/app/light/light.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { ILightDevice } from '../devices';
 import { Device } from '../device-manager.service';
 
 class LightDevice extends Device implements ILightDevice {
   rgb: boolean;
   brightness: number;
-  constructor(id: string, type: string, name?: string) {
+  on_state: boolean;
+  constructor(id: string, type: string, name?: string, on_state?: boolean) {
     super(id, type, name);
+
+    this.on_state = on_state;
   }
 }
 
@@ -23,20 +26,29 @@ export class LightComponent implements OnInit {
     return this._device;
   }
   set device(d: LightDevice){
+    console.log("setting instance")
+    console.log(d);
     this._device = d;
+    
   }
   @Output() detach: EventEmitter<string> = new EventEmitter<string>();
   rgb: boolean;
   brightness: number;
-  constructor() {
-
+  constructor(private _ref: ChangeDetectorRef) {
+    console.log("constructing component")
+    console.log(this._device);
   }
 
   ngOnInit() {
-
+    console.log("ng init")
+    console.log(this._device);
   }
 
   onClick() {
     this.detach.emit(this.device.id);
+  }
+
+  onBrightnessChange(event) {
+    this.device.on_state = true;
   }
 }
